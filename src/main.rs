c7d570a7b56012935bac7870cd36ef205fc46f16
Jack Ratner <jackratner@gmail.com>
2025-06-11 13:24:02 -0400

    parser boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use mlua::prelude::*;
 use mlua::Function;
 use mlua::Table;
-use mlua::prelude::*;
+
+mod parser;
+
 fn main() {
     // Create the lua struct for managing lua state
     let lua = Lua::new();
@@ -8,8 +11,11 @@ fn main() {
     // Setup the inputs (Just a URL for now)
     let job = lua.create_table_from([("url", "google.com")]).unwrap();
 
+    let lua_file = include_str!("../hello-world.lua");
+    let lua_sig_junk = parser::parse_lua_docs(lua_file);
+
     // Get the lua module that we're going to be using..
-    let source: Table = lua.load(include_str!("../hello-world.lua")).eval().unwrap();
+    let source: Table = lua.load(lua_file).eval().unwrap();
 
     // Call the only method we care about
     source.call_method::<()>("build_badge", job).unwrap();
---
+++ src/parser.rs
@@ -0,0 +1,8 @@
+pub struct LuaFunction {}
+
+pub fn parse_lua_docs(lua_script: &str) -> LuaFunction {
+    // 1. find build_badge fuction def line
+    // 2. find all parse lines in comments above
+
+    LuaFunction {}
+}
