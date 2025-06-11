cc2de0bb673d1df9b9428e145f6817210b60c6bc
unknown <regime_skylark.0y@icloud.com>
2025-06-11 13:00:28 -0400

    💥ADDED💥 documentation (kind of)
    for the poor pitter patter beat of code we have

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ use mlua::Function;
 use mlua::Table;
 use mlua::prelude::*;
 fn main() {
+    // Create the lua struct for managing lua state
     let lua = Lua::new();
+
+    // Setup the inputs (Just a URL for now)
     let job = lua.create_table_from([("url", "google.com")]).unwrap();
 
+    // Get the lua module that we're going to be using..
     let source: Table = lua.load(include_str!("../hello-world.lua")).eval().unwrap();
 
-    println!("{:?}", source.contains_key("build_badge"));
-
+    // Call the only method we care about
     source.call_method::<()>("build_badge", job).unwrap();
-
-    println!("Hello, world!");
 }
