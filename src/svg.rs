cfcd663d2f80c0dbc8904910b25651587128a1d7
unknown <regime_skylark.0y@icloud.com>
2025-06-19 21:43:33 -0400

    Removed the contradictory aria-hidden attribute
    I literally have a whole step to create aria attributes!!

--- src/svg.rs
+++ src/svg.rs
@@ -150,7 +150,6 @@ pub fn badgen(options: BadgerOptions) -> Result<Document, Box<dyn Error>> {
 
     // Text group
     let mut text_group = Group::new()
-        .set("aria-hidden", "true")
         .set("fill", "#fff")
         .set("text-anchor", "start")
         .set("font-family", "Verdana,DejaVu Sans,sans-serif")
