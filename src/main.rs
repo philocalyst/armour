349cb5812121ed10798a957b81de2f468fb8d2ab
miles <regime_skylark.0y@icloud.com>
2025-08-11 14:45:02 -0400

    Removed the lua integration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use std::error::Error;
 
-use mlua::Function;
-use mlua::Table;
-use mlua::prelude::*;
 use svg::BadgerOptions;
 use svg::badgen;
 
@@ -10,57 +7,16 @@ mod colors;
 mod parser;
 mod svg;
 
-fn debug_print(lua: &Lua, table: LuaTable) -> LuaResult<()> {
-    // get globals
-    let globals = lua.globals();
-
-    // Convert to string
-    match table.raw_len() {
-        0 => {
-            // Hash table - iterate through key-value pairs
-            println!("Table (hash): {{");
-            for pair in table.pairs::<LuaValue, LuaValue>() {
-                let (key, value) = pair?;
-                println!("  {:?} = {:?}", key, value);
-            }
-            println!("}}");
-        }
-        _ => {
-            // Array-like table
-            println!("Table (array): [");
-            for i in 1..=table.raw_len() {
-                let value: LuaValue = table.raw_get(i)?;
-                println!("  [{}] = {:?}", i, value);
-            }
-            println!("]");
-        }
-    }
-
-    Ok(())
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     // Create the lua struct for managing lua state
-    let lua = Lua::new();
 
-    badgen(BadgerOptions {
-        status: String::from("hi"),
-        label: Some(String::from("hello")),
+    let badge = badgen(BadgerOptions {
+        status: String::from("."),
+        label: Some(String::from("HIIIIIII")),
         ..BadgerOptions::default()
     })?;
 
-    // Setup the inputs (Just a URL for now)
-    let job = lua.create_table_from([("url", "google.com")]).unwrap();
+    std::fs::write("test.svg", badge.to_string())?;
 
-    let lua_file = include_str!("../hello-world.lua");
-    let lua_sig_junk = parser::parse_lua_docs(lua_file);
-
-    // Get the lua module that we're going to be using..
-    let source: Table = lua.load(lua_file).eval().unwrap();
-
-    // Call the only method we care about, which returns a table representing the key values found
-    let output = source.call_method::<Table>("build_badge", job).unwrap();
-
-    debug_print(&lua, output).unwrap();
     Ok(())
 }
