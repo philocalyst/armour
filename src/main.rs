98222840f8d915fd8e9dba547a81a069c7742134
miles <regime_skylark.0y@icloud.com>
2025-08-11 23:11:45 -0400

    Getting a specified function (will later be main)

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,18 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let page = project.scripts().get(0).unwrap();
 
+    let function_data: Vec<(Vec<FunctionParam>, Vec<FunctionReturn>)> = page
+        .functions
+        .iter()
+        .filter_map(|func: &FunctionDoc| {
+            if func.name == "greet" {
+                return Some((func.params.clone(), func.returns.clone()));
+            } else {
+                return None;
+            }
+        })
+        .collect();
+
     let badge = badgen(BadgerOptions {
         status: String::from("SYN"),
         label: Some(String::from("DOCS.RS")),
