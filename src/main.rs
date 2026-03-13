3e5c843245f8398594901cbe215392ed991998af
Myles Wirth <regime_skylark.0y@icloud.com>
2026-03-13 15:26:45 -0400

    Switching from Rhai to Lyon

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,21 @@
 use std::error::Error;
 
-use rhai_doc::{FunctionParam, FunctionReturn};
+use steel::SteelVal;
+use steel::steel_vm::engine::Engine;
 use svg::BadgerOptions;
 use svg::badgen;
-
-use rhai_doc::{FunctionDoc, RhaiDocBuilder};
-
-use crate::rhai::RhaiType;
 
 mod colors;
 mod rhai;
 mod svg;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let project = RhaiDocBuilder::new()
-        .with_source_dir("/home/miles/Downloads/armour/test")
-        .scan()?;
-
-    let page = project.scripts().get(0).unwrap();
-
-    let function_data: Vec<(Vec<FunctionParam>, Vec<FunctionReturn>)> = page
-        .functions
-        .iter()
-        .filter_map(|func: &FunctionDoc| {
-            if func.name == "greet" {
-                return Some((func.params.clone(), func.returns.clone()));
-            } else {
-                return None;
-            }
-        })
-        .collect();
-
-    let mut args: Vec<rhai::RhaiType> = Vec::new();
-
-    if function_data.len() > 1 {
-        println!("There should only be one main function")
-    } else {
-        let main = function_data.get(0).unwrap();
-
-        for param in main.0.clone() {
-            args.push(RhaiType::from(param.type_name.unwrap().as_str()));
-        }
-    }
-
-    println!("{:?}", args);
+    let mut steel_engine = Engine::new();
+    let answer = steel_engine.run(
+        "(+ 1 2 3 4)
+         (+ 5 6 7 8)",
+    );
+    assert_eq!(answer, Ok(vec![SteelVal::IntV(10), SteelVal::IntV(26)]));
 
     let badge = badgen(BadgerOptions {
         status: String::from("SYN"),
