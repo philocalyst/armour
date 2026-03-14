d53e6854d35a542efa45b2e511bd1cad370a1a2a
Myles Wirth <regime_skylark.0y@icloud.com>
2026-03-14 18:08:59 -0400

    Sadly ineffective markdown inserter

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
+use ::svg::Document;
+use base64::Engine as b64Engine;
+use comrak::nodes::{AstNode, NodeHtmlBlock, NodeLink, NodeValue};
+use comrak::{Arena, Options, format_commonmark, parse_document};
+
 use steel::SteelErr;
 use steel::SteelVal;
 use steel::rerrs::ErrorKind;
 use steel::steel_vm::engine::Engine;
-use steel::steel_vm::register_fn::RegisterFn;
 use tracing::{info, instrument, warn};
 
 use crate::badger::Badge;
 use crate::badger::Globals;
 use crate::error::ArmourError;
-use crate::steel_engine::setup;
 use crate::svg::{BadgerOptions, badgen};
-use crate::wrappers::toml::parse_toml;
+
+use std::fs;
+use std::io::Write;
 
 mod badger;
 mod colors;
@@ -35,9 +40,45 @@ fn main() -> Result<(), ArmourError> {
 
     let config: badger::Config = toml::from_str(include_str!("../badger.toml"))?;
 
-    process_badges(&mut engine, &config.badges, &config.globals)?;
+    let badges = process_badges(&mut engine, &config.badges, &config.globals)?;
 
+    let markdown = include_str!("../README.md");
+
+    let arena = Arena::new();
+    let mut options = Options::default();
+    options.extension.strikethrough = true;
+    options.render.r#unsafe = true;
+    options.render.escape = false;
+
+    let root = parse_document(&arena, include_str!("../README.md"), &options);
+
+    replace_badger_tags(root, badges[0].clone());
+
+    let mut markdown = String::new();
+    format_commonmark(root, &options, &mut markdown).unwrap();
+
+    fs::write("out.md", markdown);
     Ok(())
+}
+
+fn replace_badger_tags<'a>(node: &'a AstNode<'a>, svg: Document) {
+    for child in node.descendants() {
+        let mut ast = child.data.borrow_mut();
+        if let NodeValue::HtmlBlock(html) = ast.value.clone() {
+            let html = html.literal;
+            if html.starts_with("<badger") {
+                use base64::prelude::BASE64_STANDARD;
+                let encoded_svg = BASE64_STANDARD.encode(svg.to_string());
+
+                let data_uri = format!("data:image/svg+xml;base64,{}", encoded_svg);
+
+                ast.value = NodeValue::Image(Box::new(NodeLink {
+                    url: data_uri,
+                    title: String::from("hi"),
+                }));
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -74,7 +115,8 @@ fn process_badges(
     engine: &mut Engine,
     badges: &[Badge],
     globals: &Globals,
-) -> Result<(), ArmourError> {
+) -> Result<Vec<Document>, ArmourError> {
+    let mut total_badges = Vec::new();
     for badge in badges {
         let raw_entry: SteelVal =
             engine.call_function_by_name_with_args(badge.producer.entry_point(), vec![])?;
@@ -83,15 +125,15 @@ fn process_badges(
 
         info!(label = %entry.key, status = %entry.value, "generating badge");
 
-        badgen(BadgerOptions {
+        total_badges.push(badgen(BadgerOptions {
             primary_color: Some(badge.primary_color.clone()),
             secondary_color: Some(badge.secondary_color.clone()),
             label: Some(entry.key),
             status: entry.value,
             icon: None,
             scale: Some(globals.scale as f64),
-        })?;
+        })?);
     }
 
-    Ok(())
+    Ok(total_badges)
 }
